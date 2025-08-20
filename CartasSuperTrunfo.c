3e557fe5d36a7f759b407eaa60e4580e59eb9c2b
@ropsoft <rop@ropsoft.cloud>
2025-08-20 12:43:21 -0300

    finnish

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,57 @@
 #include <stdio.h>
+#include <cjson/cJSON.h>
+#include "jsonManage.h"
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    int codigo;
+    char nome[100];
+    int populacao;
+    int area;
+    int pib;
+    int pontosTuristicos;
+
+    printf("Digite o codigo da cidade: ");
+    scanf("%d", &codigo);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nome);
+    printf("Digite a populacao da cidade: ");
+    scanf("%d", &populacao);
+    printf("Digite a area da cidade: ");
+    scanf("%d", &area);
+    printf("Digite o pib da cidade: ");
+    scanf("%d", &pib);
+    printf("Digite o numero de pontos turisticos da cidade: ");
+    scanf("%d", &pontosTuristicos);
+
+    printf("Cadastrando carta...\n ------------- \n");
+
+    printf("Codigo: %d\n", codigo);
+    printf("Nome: %s\n", nome);
+    printf("Populacao: %d\n", populacao);
+    printf("Area: %d\n", area);
+    printf("Pib: %d\n", pib);
+    printf("Pontos Turisticos: %d\n", pontosTuristicos);
+
+    printf("Salvando carta...\n ------------- \n");
+
+    ArrayJSON listaDeCartas = jsonFromFile("cartas.json");
+    ObjectJSON cartaJsonObject = jsonCreateObject(
+        6,
+        "pib", 0, pib,
+        "nome", 2, nome,
+        "area", 0, area,
+        "codigo", 0, codigo,
+        "populacao", 0, populacao,
+        "pontosTuristicos", 0, pontosTuristicos);
+
+    jsonAddObject(listaDeCartas, cartaJsonObject);
+    jsonSaveToFile(listaDeCartas, "cartas.json");
+    jsonFree(listaDeCartas);
 
     return 0;
 }
